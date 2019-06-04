6d450331c6c5a089034d7c3c2f4dd67ce4669d87
Logan Kelly <logan.r.kelly@gmail.com>
2019-06-03 22:05:36 -0500

    ch7

---
+++ MixedMessages/Mixed2.java
@@ -0,0 +1,59 @@
+public class Mixed2 {
+    public static void main(String [] args) {
+        A a = new A();
+        B b = new B();
+        C c = new C();
+        A a2 = new C();
+
+        // one
+        b.m1();
+        c.m2();
+        a.m3();
+        System.out.println("");
+
+        // two
+        c.m1();
+        c.m2();
+        c.m3();
+        System.out.println("");
+
+        // three
+        a.m1();
+        b.m2();
+        c.m3();
+        System.out.println("");
+
+        // four
+        a2.m1();
+        a2.m2();
+        a2.m3();
+        System.out.println("");
+
+    }
+}
+
+class A {
+    int ivar = 7;
+    void m1() {
+        System.out.print("A's m1, ");
+    }
+    void m2() {
+        System.out.print("A's m2, ");
+    }
+    void m3() {
+        System.out.print("A's m3, ");
+    }
+}
+
+class B extends A {
+    void m1() {
+        System.out.print("B's m1, ");
+    }
+}
+
+class C extends B {
+    void m3() {
+        System.out.print("C's m3, " + (ivar + 6));
+    }
+}
+
