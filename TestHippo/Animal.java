441cf6a2d60aba5c18497352abbd53ad5718a0a8
Logan Kelly <logan.r.kelly@gmail.com>
2019-06-06 23:26:48 -0500

    Naming a hippo.

--- TestHippo/Animal.java
+++ TestHippo/Animal.java
@@ -1,5 +1,14 @@
 class Animal {
-    public Animal() {
+    private String name;
+    private String type;
+
+    public Animal(String name, String type) {
+        this.name = name;
+        this.type = type;
         System.out.println("Making an Animal.");
+    }
+
+    public void sayName() {
+        System.out.println(type + " named " + name + ".");
     }
 }
--- TestHippo/TestHippo.java
+++ TestHippo/TestHippo.java
@@ -1,6 +1,7 @@
 class TestHippo {
     public static void main (String[] args) {
         System.out.println("Initializing hippopotomous...");
-        Hippo hippo = new Hippo();
+        Hippo hippo = new Hippo("Teddy Bear");
+        hippo.sayName();
     }
 }
